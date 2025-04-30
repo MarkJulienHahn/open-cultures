21b8200916a088386b9f79125ca870f78cfa89b2
Mark Julien Hahn <mail@markjulienhahn.de>
2025-04-30 15:52:32 +0200

    ..

--- src/app/(homepage)/page.tsx
+++ src/app/(homepage)/page.tsx
@@ -2,14 +2,17 @@ import News from "@/components/home/News";
 import OpenCultures from "@/components/home/OpenCultures";
 import Projects from "@/components/home/Projects";
 import Team from "@/components/home/Team";
+import { Suspense } from "react";
 
 export default function Home() {
   return (
     <main>
-      <News />
-      <OpenCultures />
-      <Team />
-      <Projects />
+      <Suspense fallback={<div>Loading...</div>}>
+        <News />
+        <OpenCultures />
+        <Team />
+        <Projects />
+      </Suspense>
     </main>
   );
 }
