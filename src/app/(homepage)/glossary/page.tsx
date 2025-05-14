62d7d81db597aaee248ed2a763158aafc54043e6
Mark Julien Hahn <mail@markjulienhahn.de>
2025-05-14 17:58:59 +0200

    ...2

--- src/app/(homepage)/glossary/page.tsx
+++ src/app/(homepage)/glossary/page.tsx
@@ -83,97 +83,107 @@ export default async function Glossary() {
             spokenWordProjects={spokenWordProjects}
           />
         </Suspense>
-        <IndexRow
-          label={"Open Planning Cultures"}
-          category="open-planning-culture"
-          introtext={openCultures.introtext.text}
-        />
-        <p className={styles.section}>SubLabs</p>
-        <IndexRow
-          label={"Open Planning"}
-          category="open-planning"
-          introtext={openPlanning.introtext.text}
-        />
-        <IndexRow
-          label={"Open Factory"}
-          category="open-factory"
-          introtext={openFactory.introtext.text}
-        />
-        <IndexRow
-          label={"Open Imaginaries"}
-          category="open-imaginaries"
-          introtext={openImaginaries.introtext.text}
-        />
+        <Suspense fallback={null}>
+          <IndexRow
+            label={"Open Planning Cultures"}
+            category="open-planning-culture"
+            introtext={openCultures.introtext.text}
+          />
+          <p className={styles.section}>SubLabs</p>
+          <IndexRow
+            label={"Open Planning"}
+            category="open-planning"
+            introtext={openPlanning.introtext.text}
+          />
+          <IndexRow
+            label={"Open Factory"}
+            category="open-factory"
+            introtext={openFactory.introtext.text}
+          />
+          <IndexRow
+            label={"Open Imaginaries"}
+            category="open-imaginaries"
+            introtext={openImaginaries.introtext.text}
+          />
+        </Suspense>
         <p className={styles.section}>People</p>
-        <div className={styles.rows}>
-          <IndexRow
-            label={"Team"}
-            content={team}
-            category="team"
-            filter={true}
-          />
-          <IndexRow
-            label={"Praxis Partners"}
-            content={praxisPartners}
-            category="praxis-partners"
-          />
-          <IndexRow
-            label={"Advisory Board"}
-            content={advisoryBoard}
-            category="advisory-board"
-          />
-        </div>
+        <Suspense fallback={null}>
+          <div className={styles.rows}>
+            <IndexRow
+              label={"Team"}
+              content={team}
+              category="team"
+              filter={true}
+            />
+            <IndexRow
+              label={"Praxis Partners"}
+              content={praxisPartners}
+              category="praxis-partners"
+            />
+            <IndexRow
+              label={"Advisory Board"}
+              content={advisoryBoard}
+              category="advisory-board"
+            />
+          </div>
+        </Suspense>
         <p className={styles.section}>LivingLab</p>
-        <div className={styles.rows}>
-          <IndexRow
-            label={"LivingLab"}
-            category="living-lab"
-            introtext={livingLab.text}
-            content={livingLabProjects}
-          />
-        </div>
+        <Suspense fallback={null}>
+          <div className={styles.rows}>
+            <IndexRow
+              label={"LivingLab"}
+              category="living-lab"
+              introtext={livingLab.text}
+              content={livingLabProjects}
+            />
+          </div>
+        </Suspense>
         <p className={styles.section}>Interacting</p>
-        <div className={styles.rows}>
-          <IndexRow
-            label={"Transdisciplinary Method Lab"}
-            category="transdisciplinary-method-lab"
-            introtext={tml.text}
-            content={tmlProjects}
-          />
-          <IndexRow
-            label={"Methodological PhD Colloquium"}
-            category="methodological-phd-colloquium"
-            introtext={mpc.text}
-            content={mpcProjects}
-          />
-          <IndexRow
-            label={"Summer Schools"}
-            category="summer-schools"
-            introtext={summerSchools.text}
-            content={summerSchoolProjects}
-          />
-          <IndexRow
-            label={"Conferences"}
-            category="conferences"
-            introtext={conferences.text}
-            content={conferencesProjects}
-          />
-        </div>
+        <Suspense fallback={null}>
+          <div className={styles.rows}>
+            <IndexRow
+              label={"Transdisciplinary Method Lab"}
+              category="transdisciplinary-method-lab"
+              introtext={tml.text}
+              content={tmlProjects}
+            />
+            <IndexRow
+              label={"Methodological PhD Colloquium"}
+              category="methodological-phd-colloquium"
+              introtext={mpc.text}
+              content={mpcProjects}
+            />
+            <IndexRow
+              label={"Summer Schools"}
+              category="summer-schools"
+              introtext={summerSchools.text}
+              content={summerSchoolProjects}
+            />
+            <IndexRow
+              label={"Conferences"}
+              category="conferences"
+              introtext={conferences.text}
+              content={conferencesProjects}
+            />
+          </div>
+        </Suspense>
         <p className={styles.section}>Mediating</p>
-        <div className={styles.rows}>
-          <IndexRow
-            label={"Printed Matter"}
-            category="printed-matter"
-            introtext={printedMatter.text}
-            content={printedMatterProjects}
-          />
-          <IndexRow
-            label={"Spoken Word"}
-            category="spoken-word"
-            introtext={spokenWord.text}
-            content={spokenWordProjects}
-          />
-        </div>
+        <Suspense fallback={null}>
+          <div className={styles.rows}>
+            <IndexRow
+              label={"Printed Matter"}
+              category="printed-matter"
+              introtext={printedMatter.text}
+              content={printedMatterProjects}
+            />
+            <IndexRow
+              label={"Spoken Word"}
+              category="spoken-word"
+              introtext={spokenWord.text}
+              content={spokenWordProjects}
+            />
+          </div>
+        </Suspense>
         <Wallpaper background={"beige"} img={""} />
       </main>
       <Footer />
