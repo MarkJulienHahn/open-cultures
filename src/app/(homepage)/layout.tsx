3deb6172985388b0c81e55a8cdd1b18d2a63f38f
Mark Julien Hahn <mail@markjulienhahn.de>
2025-07-03 14:42:28 +0200

    ...

--- src/app/(homepage)/layout.tsx
+++ src/app/(homepage)/layout.tsx
@@ -7,6 +7,30 @@ export const metadata: Metadata = {
   title: "OpenCultures",
   description:
     "The overall aim of Open Planning Cultures. Design Principles for Transformative Spaces (OPEN_CULTURES) is to develop climate-sensitive design principles for urban development and spatial planning that are both based on and foster the translation of climate knowledge into the everyday practices of citizens to engender sustainable forms of urban living.",
+  openGraph: {
+    title: "OpenCultures",
+    description:
+      "Design principles for climate-sensitive urban development based on citizens’ everyday practices.",
+    url: "https://www.opencultures.org",
+    siteName: "OpenCultures",
+    images: [
+      {
+        url: "https://opencultures.org/oc-image.png",
+        width: 1200,
+        height: 830,
+        alt: "OpenCultures Preview Image",
+      },
+    ],
+    locale: "en_US",
+    type: "website",
+  },
+  twitter: {
+    card: "summary_large_image",
+    title: "OpenCultures",
+    description:
+      "Design principles for climate-sensitive urban development based on citizens’ everyday practices.",
+    images: ["https://opencultures.org/oc-image.png"],
+  },
 };
 
 export default function RootLayout({
@@ -16,10 +40,10 @@ export default function RootLayout({
 }) {
   return (
     <html lang="en">
-        <body>
-          <Nav />
-          {children}
-        </body>
+      <body>
+        <Nav />
+        {children}
+      </body>
     </html>
   );
 }
